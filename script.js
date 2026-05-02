0d8c716039f60a04ab6f3bef79d6a75d47df4a47
MXDXHugh <19951754337g@gmail.com>
2026-05-02 16:12:36 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,40 +2,6 @@ const lightbox = document.querySelector(".lightbox");
 const lightboxImage = document.querySelector(".lightbox-image");
 const closeButton = document.querySelector(".lightbox-close");
 
-const clickables = document.querySelectorAll("a, button");
-
-clickables.forEach((element) => {
-  element.addEventListener("click", (event) => {
-    element.classList.add("is-clicking");
-    window.setTimeout(() => element.classList.remove("is-clicking"), 150);
-
-    if (!(element instanceof HTMLAnchorElement)) {
-      return;
-    }
-
-    const href = element.getAttribute("href");
-    const isModifiedClick =
-      event.metaKey || event.ctrlKey || event.shiftKey || event.altKey || event.button !== 0;
-    const shouldStayOnPage =
-      !href ||
-      href.startsWith("#") ||
-      href.startsWith("mailto:") ||
-      href.startsWith("http") ||
-      element.target ||
-      isModifiedClick;
-
-    if (shouldStayOnPage) {
-      return;
-    }
-
-    event.preventDefault();
-    document.body.classList.add("is-leaving");
-    window.setTimeout(() => {
-      window.location.href = element.href;
-    }, 140);
-  });
-});
-
 document.querySelectorAll("[data-full]").forEach((button) => {
   button.addEventListener("click", () => {
     const image = button.querySelector("img");
